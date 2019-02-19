ba9969d0924e03b3888b9b7c98c6d4c042fcba8c
Fabio Streun <fstreun@student.ethz.ch>
2019-02-19 11:53:00 +0100

    moved termination checks to termination-plugin

---
+++ src/main/scala/viper/termination/DecreasesSimple.scala
@@ -0,0 +1,13 @@
+package viper.termination
+
+import viper.silver.ast
+import viper.silver.ast.Program
+import viper.silver.plugin.termination.checkcode.{CheckDecreasesSimple, DecreasesExp}
+
+
+class DecreasesSimple extends DecreasesPlugin {
+  override def transformToCheckProgram(input: Program, decreasesMap: Map[ast.Function, DecreasesExp]): Program = {
+    val termCheck = new CheckDecreasesSimple(input, decreasesMap)
+    termCheck.createCheckProgram()
+  }
+}
