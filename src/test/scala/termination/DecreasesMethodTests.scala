7e8c1629f93a40d763d91092ca902481956f9915
Fabio Streun <fstreun@student.ethz.ch>
2019-03-15 19:16:54 +0100

    moved tests

---
+++ src/test/scala/termination/DecreasesMethodTests.scala
@@ -0,0 +1,9 @@
+
+package viper.termination
+
+import viper.silicon.tests.SiliconTests
+
+class DecreasesMethodTests extends SiliconTests {
+  override val testDirectories: Seq[String] = Seq("termination/methods")
+  override val commandLineArguments: Seq[String] = Seq("--plugin", "viper.termination.DecreasesMethod")
+}
